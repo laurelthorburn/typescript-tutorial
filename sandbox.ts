bcf4c62c520308f57bf7146e70b95facc957321b
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-15 18:23:36 -0600

    adding notes

--- sandbox.ts
+++ sandbox.ts
@@ -1,14 +1,15 @@
 let character = 'mario';
 let age = 30;
 let isBlackBelt = false;
+/// can change values, but not type
 
-// character = 20;
+// character = 20; can't change to number because it's OG is a string. can only change to another string. it's type sensitive
 character = 'luigi';
 
-// age = 'yoshi';
+// age = 'yoshi'; must be number
 age = 40;
 
-// isBlackBelt = 'yes';
+// isBlackBelt = 'yes'; can't be string but be boolean 
 isBlackBelt = true;
 
 const circ = (diameter: number) => {
